d2107e9a5ae0e4cb82a17569de04c9c82ada2e70
Haruka Ogino <harukariq@gmail.com>
2024-04-16 08:54:58 +1200

    set up jigsaw puzzle pages&routes

---
+++ client/pages/Puzzle.tsx
@@ -0,0 +1,12 @@
+import EasyPuzzle from './Puzzle/EasyPuzzle'
+
+export default function Puzzle() {
+  return (
+    <>
+      <div className="puzzle-page">
+        <h1>This is the puzzle page!</h1>
+        <EasyPuzzle />
+      </div>
+    </>
+  )
+}
---
+++ client/pages/Puzzle/EasyPuzzle.tsx
@@ -0,0 +1,9 @@
+export default function EasyPuzzle() {
+  return (
+    <>
+      <div className="puzzle-type">
+        <h2>Easy puzzles live here</h2>
+      </div>
+    </>
+  )
+}
