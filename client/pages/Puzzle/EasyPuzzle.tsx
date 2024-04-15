23e1676f89bf6d407d001a50500e28fab4d9b6b6
Haruka Ogino <harukariq@gmail.com>
2024-04-16 09:22:26 +1200

    display puzzle img issues

--- client/pages/Puzzle/EasyPuzzle.tsx
+++ client/pages/Puzzle/EasyPuzzle.tsx
@@ -1,8 +1,18 @@
 export default function EasyPuzzle() {
+  const imageNumbers = Array.from({ length: 15 }, (_, index) => index + 1)
   return (
     <>
       <div className="puzzle-type">
         <h2>Easy puzzles live here</h2>
+        {imageNumbers.map((number, i) => (
+          <img
+            src={
+              number < 10 ? `/image_part_00${number}` : `/image_part_0${number}`
+            }
+            alt="puzzle piece"
+            key={i}
+          />
+        ))}
       </div>
     </>
   )
