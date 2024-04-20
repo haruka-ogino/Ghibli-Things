8d479f6fba5348c502410f37f616fe2b38a461d5
Haruka Ogino <harukariq@gmail.com>
2024-04-20 22:49:40 +1200

    update imports of jigsaw

--- client/pages/Puzzle.tsx
+++ client/pages/Puzzle.tsx
@@ -1,11 +1,11 @@
-import EasyPuzzle from './Puzzle/EasyPuzzle'
+import JigsawPuzzle from './Puzzle/JigsawPuzzle'
 
 export default function Puzzle() {
   return (
     <>
       <div className="puzzle-page">
         <h1>This is the puzzle page!</h1>
-        <EasyPuzzle />
+        <JigsawPuzzle />
       </div>
     </>
   )
