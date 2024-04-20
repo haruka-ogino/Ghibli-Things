aa407da4e03d62f3f031e8e26a38deccf922da61
Haruka Ogino <harukariq@gmail.com>
2024-04-20 23:03:03 +1200

    set up div styling for puzzle pieces

--- client/pages/Puzzle/JigsawPuzzle.tsx
+++ client/pages/Puzzle/JigsawPuzzle.tsx
@@ -1,21 +1,26 @@
+import '../../styles/jigsaw.css'
+
 export default function JigsawPuzzle() {
   const imageNumbers = Array.from({ length: 15 }, (_, index) => index + 1)
 
   return (
     <>
       <div className="puzzle-type">
-        <h2>Easy puzzles live here</h2>
-        {imageNumbers.map((number, i) => (
-          <img
-            src={
-              number < 10
-                ? `/images/soot-parts-easy/image_part_00${number}.png`
-                : `/images/soot-parts-easy/image_part_0${number}.png`
-            }
-            alt="puzzle piece"
-            key={i}
-          />
-        ))}
+        <h2>Jigsaw puzzles live here</h2>
+        <p>The pieces are below</p>
+        <div className="jigsaw-pieces">
+          {imageNumbers.map((number, i) => (
+            <img
+              src={
+                number < 10
+                  ? `/images/soot-parts-easy/image_part_00${number}.png`
+                  : `/images/soot-parts-easy/image_part_0${number}.png`
+              }
+              alt="puzzle piece"
+              key={i}
+            />
+          ))}
+        </div>
       </div>
     </>
   )
