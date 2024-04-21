aa68f5bfcfe54a4f0352ed7b9cdab14d9ea7a587
Haruka Ogino <harukariq@gmail.com>
2024-04-21 18:11:40 +1200

    tidy up

--- client/pages/Puzzle/JigsawPuzzle.tsx
+++ client/pages/Puzzle/JigsawPuzzle.tsx
@@ -5,14 +5,8 @@ import PuzzlePieces from './PuzzlePieces'
 
 export default function JigsawPuzzle() {
   const imageNumbers = Array.from({ length: 15 }, (_, index) => index + 1)
-  const [board, setBoard] = useState([])
-
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: 'image',
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }))
+  const initialState = Array.from({ length: 15 }, () => '')
+  const [board, setBoard] = useState(initialState)
 
   function handleUrl(number: number): string {
     if (number < 10) {
@@ -29,29 +23,12 @@ export default function JigsawPuzzle() {
         <p>puzzle board is below</p>
         <div className="board">
           {imageNumbers.map((number, i) => (
-            // <div key={i} className="box">
-            //   {/* p */}
-            // </div>
-            <p key={i} className="box">
-              p
-            </p>
+            <p key={i} className="box"></p>
           ))}
         </div>
         <p>The pieces are below</p>
         <div className="jigsaw-pieces">
           {imageNumbers.map((number, i) => (
-            // <img
-            //   ref={drag}
-            //   style={{ border: isDragging ? '2px solid pink' : '0px' }}
-            //   className="piece"
-            //   src={
-            //     number < 10
-            //       ? `/images/soot-parts-easy/image_part_00${number}.png`
-            //       : `/images/soot-parts-easy/image_part_0${number}.png`
-            //   }
-            //   alt="puzzle piece"
-            //   key={i}s
-            // />
             // eslint-disable-next-line react/jsx-key
             <PuzzlePieces url={handleUrl(number)} i={i} />
           ))}
