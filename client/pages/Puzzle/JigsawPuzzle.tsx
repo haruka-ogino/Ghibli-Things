d46598894674d3436702cbd267a9c4c75df82a53
Haruka Ogino <harukariq@gmail.com>
2024-04-21 17:18:08 +1200

    make puzzle board

--- client/pages/Puzzle/JigsawPuzzle.tsx
+++ client/pages/Puzzle/JigsawPuzzle.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import '../../styles/jigsaw.css'
 
 export default function JigsawPuzzle() {
   const imageNumbers = Array.from({ length: 15 }, (_, index) => index + 1)
+  const [board, setBoard] = useState([])
 
   return (
     <>
       <div className="puzzle-type">
         <h2>Jigsaw puzzles live here</h2>
+        <p>puzzle board is below</p>
+        <div className="board">
+          {imageNumbers.map((number, i) => (
+            // <div key={i} className="box">
+            //   {/* p */}
+            // </div>
+            <p key={i} className="box">
+              p
+            </p>
+          ))}
+        </div>
         <p>The pieces are below</p>
         <div className="jigsaw-pieces">
           {imageNumbers.map((number, i) => (
