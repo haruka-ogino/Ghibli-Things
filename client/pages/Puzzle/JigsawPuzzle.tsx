5e2986d3896787bdf1c397ce2033d82102c62ce9
Haruka Ogino <harukariq@gmail.com>
2024-04-25 11:16:30 +1200

    tidy up - remove commented code

--- client/pages/Puzzle/JigsawPuzzle.tsx
+++ client/pages/Puzzle/JigsawPuzzle.tsx
@@ -8,26 +8,6 @@ export default function JigsawPuzzle() {
   const [pieces, setPieces] = useState(initialPieces)
   const initialState = Array.from({ length: 15 }, () => 0)
   const [board, setBoard] = useState(initialState)
-  // const [{ isOver }, drop] = useDrop(() => ({
-  //   accept: 'image',
-  //   drop: (item: { number: number }, monitor) =>
-  //     placePiece(item.number, monitor),
-  //   collect: (monitor) => ({
-  //     isOver: !!monitor.isOver(),
-  //   }),
-  // }))
-
-  // function placePiece(number: number, monitor: DropTargetMonitor) {
-  //   if (number < 10) {
-  //     console.log(`/images/soot-parts-easy/image_part_00${number}.png`)
-  //   } else {
-  //     console.log(`/images/soot-parts-easy/image_part_0${number}.png`)
-  //   }
-  //   const dropIndex: number = monitor.getItem().index // Get the index of the drop target
-  //   const updatedBoard = [...board]
-  //   updatedBoard[dropIndex] = number // Update the board array at the drop index
-  //   setBoard(updatedBoard)
-  // }
 
   for (let i = initialPieces.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
