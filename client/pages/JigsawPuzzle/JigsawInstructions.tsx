00919eb20668ec07a47c807ddc541d9e52bed46f
Haruka Ogino <harukariq@gmail.com>
2024-05-01 16:31:29 +1200

    update instructions

--- client/pages/JigsawPuzzle/JigsawInstructions.tsx
+++ client/pages/JigsawPuzzle/JigsawInstructions.tsx
@@ -7,15 +7,23 @@ export default function JigsawInstructions({ setShow }: Props) {
   return (
     <>
       <div className="popup-overlay" id="jigsaw-overlay">
-        <div className="popup">
+        <div className="popup" style={{ width: '45em' }}>
           <button id="close" onClick={() => setShow(false)}>
             x
           </button>
           <h2>Instructions</h2>
-          <ul>
-            <li>To place a piece, drag it onto the puzzle board.</li>
-            {/* <li>To remove a piece from the puzzle board, click it once.</li> */}
-            <li>Use the clue button and have fun!</li>
+          <ul style={{ textAlign: 'left' }}>
+            <li>
+              - To place a piece, click the piece and click on the place in the
+              board where you wish to place it.
+            </li>
+            <li>- To remove a piece from the puzzle board, click it once.</li>
+            <li>
+              - You may also place a piece where another piece has already been
+              placed on the puzzle board, and this will be return to the puzzle
+              board.
+            </li>
+            <li>- Use the clue button if you need it and have fun!</li>
           </ul>
         </div>
       </div>
