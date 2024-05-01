5c50685e6e85bb1dde8ef8a93aa7747a6f535432
Haruka Ogino <harukariq@gmail.com>
2024-05-01 16:21:16 +1200

    remove commented code

--- client/pages/JigsawPuzzle/JigsawPuzzle.tsx
+++ client/pages/JigsawPuzzle/JigsawPuzzle.tsx
@@ -29,14 +29,6 @@ export default function JigsawPuzzle() {
     ;[initialPieces[i], initialPieces[j]] = [initialPieces[j], initialPieces[i]]
   }
 
-  // function handleUrl(number: number): string {
-  //   if (number < 10) {
-  //     return `/images/soot-parts-easy/image_part_00${number}.png` as string
-  //   } else {
-  //     return `/images/soot-parts-easy/image_part_0${number}.png` as string
-  //   }
-  // }
-
   function restartGame() {
     setPieces(initialPieces)
     setBoard(initialState)
--- client/pages/JigsawPuzzle/PuzzlePieces.tsx
+++ client/pages/JigsawPuzzle/PuzzlePieces.tsx
@@ -1,21 +1,9 @@
-// import { useDrag } from 'react-dnd'
-
 interface Props {
   number: number
   setPiece: React.Dispatch<React.SetStateAction<number>>
 }
 
 export default function PuzzlePieces({ number, setPiece }: Props) {
-  // const [{ isDragging }, drag] = useDrag(() => ({
-  //   type: 'image',
-  //   item: { type: 'image', number: number },
-  //   collect: (monitor) => ({
-  //     isDragging: !!monitor.isDragging(),
-  //   }),
-  // }))
-
-  // console.log(number)
-
   function handleUrl(number: number): string {
     if (number < 10) {
       return `/images/soot-parts-easy/image_part_00${number}.png` as string
@@ -33,10 +21,6 @@ export default function PuzzlePieces({ number, setPiece }: Props) {
     <>
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
       <img
-        // ref={number === 90 ? null : drag}
-        // style={{
-        //   border: isDragging ? '0.5em solid rgb(56, 158, 163)' : '0px',
-        // }}
         className="piece"
         src={handleUrl(number)}
         alt="puzzle piece"
