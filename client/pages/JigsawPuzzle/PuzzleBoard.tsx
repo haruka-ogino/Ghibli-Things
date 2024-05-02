34bb02e0bb6a12f1f0c61bfec2ba3a823fd7c87c
Haruka Ogino <harukariq@gmail.com>
2024-05-02 22:08:34 +1200

    debug moving div

--- client/pages/JigsawPuzzle/PuzzleBoard.tsx
+++ client/pages/JigsawPuzzle/PuzzleBoard.tsx
@@ -128,7 +128,7 @@ export default function PuzzleBoard({
   }
 
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/no-static-element-interactions
     <p
       className={`box ${isOver ? 'drop-position' : ''}`}
       ref={drop}
