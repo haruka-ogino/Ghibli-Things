e2a21116a9202271ebad1c1523691a3f6b5ec734
Haruka Ogino <harukariq@gmail.com>
2024-05-02 12:16:36 +1200

    set up Board component

--- client/pages/DragDrop.tsx
+++ client/pages/DragDrop.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import Pieces from './DragNDrop/Pieces'
+import Board from './DragNDrop/Board'
 
 export default function DragDrop() {
   const initialState = ['helloooooooooooooooooo', 'holaaaaaaaaaaaaaaaaa']
   const [pieces, setPieces] = useState(initialState)
+  const initialBoard: string[] = ['']
+  const [board, setBoard] = useState(initialBoard)
 
   return (
     <>
@@ -11,6 +14,12 @@ export default function DragDrop() {
         <h1>hey hey dragging and dropping here</h1>
         {pieces.map((piece, i) => (
           <Pieces key={i} i={i} piece={piece} />
+        ))}
+      </div>
+      <h1>Board is bellow</h1>
+      <div style={{ border: '2px solid blue', height: '50px' }}>
+        {board.map((thing, i) => (
+          <Board key={i} i={i} thing={thing} />
         ))}
       </div>
     </>
--- client/pages/DragNDrop/Board.tsx
+++ client/pages/DragNDrop/Board.tsx
@@ -1,11 +1,19 @@
 import { useDrop } from 'react-dnd'
-
-export default function Board() {
-  const [{ isOver }, drop] = useDrop(() => ({
-    accept: 'image',
-    drop: (item: { number: number }) => placePiece(item.piece),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
-  }))
+interface Props {
+  thing: string
+  i: number
+}
+export default function Board({ thing, i }: Props) {
+  // const [{ isOver }, drop] = useDrop(() => ({
+  //   accept: 'image',
+  //   drop: (item: { number: number }) => placePiece(item.piece),
+  //   collect: (monitor) => ({
+  //     isOver: !!monitor.isOver(),
+  //   }),
+  // }))
+  return (
+    <>
+      <div>{thing}</div>
+    </>
+  )
 }
