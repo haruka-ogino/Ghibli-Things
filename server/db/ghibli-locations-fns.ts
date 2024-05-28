d09c7136a8b162f57383e424bc020a2e605549c2
Haruka Ogino <harukariq@gmail.com>
2024-05-28 15:34:26 +1200

    rename some data fields on BE

--- server/db/ghibli-locations-fns.ts
+++ server/db/ghibli-locations-fns.ts
@@ -1,5 +1,12 @@
 import db from './connection.ts'
 
 export async function getAllLocations() {
-  return await db('locations').select()
+  return await db('locations')
+    // .join('films', 'films.id', 'locations.film_id')
+    .select(
+      'locations.id as id',
+      'film_id as film',
+      'locations.image_url as imgUrl',
+      'locations.description as description',
+    )
 }
