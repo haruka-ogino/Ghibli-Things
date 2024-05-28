e0c9e1e595698b2eccbf4ad6a4dc9e642b896710
Haruka Ogino <harukariq@gmail.com>
2024-05-28 15:54:03 +1200

    create table to display data on FE

--- client/pages/Locations.tsx
+++ client/pages/Locations.tsx
@@ -9,11 +9,34 @@ export default function Locations() {
   if (isError) return <h1>Error retrieving data.</h1>
 
   if (locations) {
-    console.log(locations)
     return (
       <>
         <h1>Real Life Locations</h1>
         <p>{location}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Film</th>
+              <th>Description</th>
+              <th>Rating</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {locations.map((location, i) => (
+              <tr key={i}>
+                <td>{location.name}</td>
+                <td>{location.film}</td>
+                <td>{location.description}</td>
+                <td>{location.rating}</td>
+                <td>
+                  <a href={location.url}>{location.address}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </>
     )
   }
