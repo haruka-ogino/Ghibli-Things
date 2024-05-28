6fe1f9ba929497cbff1b198cd41d4afee15bb7a7
Haruka Ogino <harukariq@gmail.com>
2024-05-28 15:17:36 +1200

    retrieve data from BE

--- client/pages/Locations.tsx
+++ client/pages/Locations.tsx
@@ -1,9 +1,20 @@
+import useGetLocations from '../hooks/useLocations'
+
 export default function Locations() {
-  const locations = 'hey'
-  return (
-    <>
-      <p>locations</p>
-      <p>{locations}</p>
-    </>
-  )
+  const location = 'hey'
+  const { data: locations, isLoading, isError } = useGetLocations()
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (isError) return <h1>Error retrieving data.</h1>
+
+  if (locations) {
+    console.log(locations)
+    return (
+      <>
+        <h1>Real Life Locations</h1>
+        <p>{location}</p>
+      </>
+    )
+  }
 }
