f4c791026c2e5ee7d5a2ae515eb691ef6653f195
Haruka Ogino <harukariq@gmail.com>
2024-05-28 16:08:42 +1200

    "left-join" tables on the BE

--- server/db/ghibli-locations-fns.ts
+++ server/db/ghibli-locations-fns.ts
@@ -2,10 +2,10 @@ import db from './connection.ts'
 
 export async function getAllLocations() {
   return await db('locations')
-    // .join('films', 'films.id', 'locations.film_id')
+    .leftJoin('films', 'films.id', 'locations.film_id')
     .select(
       'locations.id as id',
-      'film_id as film',
+      'films.title as film',
       'locations.image_url as imgUrl',
       'locations.description as description',
     )
