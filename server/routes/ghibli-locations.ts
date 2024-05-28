971c3fa007d4249d03dbe12590be2bc37eb1a972
Haruka Ogino <harukariq@gmail.com>
2024-05-28 13:05:37 +1200

    tidy up

--- server/routes/ghibli-locations.ts
+++ server/routes/ghibli-locations.ts
@@ -6,21 +6,10 @@ import 'dotenv/config'
 
 const router = Router()
 
-console.log(process.env.GOOGLE_KEY)
-
 // '/api/v1/ghibli/locations'
 
 router.get('/', async (req, res) => {
   try {
-    // const placeId = 'ChIJmbzCFl_jGGAR98V28VrP0QI'
-    // const response = await axios.get(
-    //   `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${process.env.GOOGLE_KEY}`,
-    // )
-    // rating = response.data.result.rating
-    // name = response.data.result.name
-    // address = response.data.result.formatted_address
-    // url = response.data.result.url
-
     const locations = await db.getAllLocations()
     const locationData = await Promise.all(
       locations.map(async (location) => {
